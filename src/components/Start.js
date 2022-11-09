66559b602567a471b5ce10f7f64dd5f040738d97
alok <alok60541@gmail.com>
2022-11-09 22:43:57 +0530

    start functionality

--- src/components/Start.js
+++ src/components/Start.js
@@ -1,9 +1,21 @@
+import {useRef} from 'react';
 
-export default function Start() {
+export default function Start({setUsername}) {
+    const inputRef = useRef();
+
+    const handleClick = ()=> {
+        inputRef.current.value && setUsername(inputRef.current.value);
+    };
+
     return (
         <div className="start">
-            <input placeholder="enter your name" className="startInput" />
-            <button className="startButton">Start</button>
+            <input 
+                placeholder="enter your name" 
+                className="startInput" 
+                ref={inputRef}
+            />
+
+            <button className="startButton" onClick={handleClick}>Start</button>
         </div>
     )
 }
